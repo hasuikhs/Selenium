4e33d0470bb9ab3d538c22edad3bc0e973d34578
Hanseok <hasuikhs@gmail.com>
2020-10-18 19:47:02 +0900

    201018|Update naver login

--- with nodejs/index.js
+++ with nodejs/index.js
@@ -2,12 +2,13 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
 
 async function example() {
     let driver = await new Builder().forBrowser('chrome').build();
-    try {
-        await driver.get('http://www.google.com/ncr');
-        await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-        await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-    } finally {
-        await driver.quit();
-    }
+        await driver.get('https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com');
+        await driver.findElement(By.id('id')).sendKeys('id', Key.RETURN);
+        await driver.findElement(By.id('pw')).sendKeys('pass', Key.RETURN);
+
+        // await driver.findElement(By.name('q')).sendKeys('Selenium', Key.ENTER);
+        
+    // var html = await driver.getPageSource();
+    // console.log(html)
 }
 example();
