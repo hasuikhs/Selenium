73b4e9c43be726681bf4aed61b2fecfe79edd5cd
Hanseok Kim <hasuikhs@gmail.com>
2020-10-19 13:22:16 +0900

    201019|Update selenium response

--- with nodejs/index.js
+++ with nodejs/index.js
@@ -1,10 +1,11 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const cheerio = require('cheerio');
+const request = require('request');
 
 async function example() {
     let driver = await new Builder().forBrowser('chrome').build();
 
-    await driver.get('https://auth.ncloud.com/nsa');
+    await driver.get('https://auth.ncloud.com/nsa/');
     await driver.findElement(By.id('username')).sendKeys('id', Key.RETURN);
     await driver.findElement(By.id('passwordPlain')).sendKeys('pw', Key.RETURN);
 
@@ -14,9 +15,33 @@ async function example() {
     // var text = $('#app > div > header > div.center-wrap > div.fr > div.util.hidden-sm-down > a').text();
 
     await driver.findElement(By.css('#app > div > header > div.center-wrap > div.fr > div.util.hidden-sm-down > a')).click();
-    var apiKey = await driver.manage().getCookie('ncp_nnb');
+    var apiKey = await driver.manage().getCookie('ncp');
 
     console.log(apiKey.value);
+
+    const option = {
+        uri: 'https://monitoring-api.ncloud.com/monapi/pfmnc',
+        method: 'POST',
+        headers : {
+            'X-NCP-access-token' : apiKey.value
+        },
+        body: {
+            "targets": [
+                { 
+                    "metric": "avg.svr.cpu.used.rto",
+                    "name": "avg.svr.cpu.used.rto____0", 
+                    "key": "F2:20:CD:AD:38:85", 
+                    "startTime": "202010191159", 
+                    "endTime": "202010191259" }
+                ]
+        },
+        json: true
+    }
+    request.post(option, function (error, response, body) {
+        console.log(response.body.csv);
+    });
+
+
     // console.log(html)
 }
 example();
