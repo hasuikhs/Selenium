ec4c80e88b8e4125d8286a4388b6b43ae331e69f
Hanseok Kim <hasuikhs@gmail.com>
2020-10-19 12:46:28 +0900

    201019|Update use cheerio, get cookie

--- with nodejs/index.js
+++ with nodejs/index.js
@@ -1,14 +1,22 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
+const cheerio = require('cheerio');
 
 async function example() {
     let driver = await new Builder().forBrowser('chrome').build();
-        await driver.get('https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com');
-        await driver.findElement(By.id('id')).sendKeys('id', Key.RETURN);
-        await driver.findElement(By.id('pw')).sendKeys('pass', Key.RETURN);
 
-        // await driver.findElement(By.name('q')).sendKeys('Selenium', Key.ENTER);
-        
-    // var html = await driver.getPageSource();
+    await driver.get('https://auth.ncloud.com/nsa');
+    await driver.findElement(By.id('username')).sendKeys('id', Key.RETURN);
+    await driver.findElement(By.id('passwordPlain')).sendKeys('pw', Key.RETURN);
+
+    var html = await driver.getPageSource();
+
+    // var $ = cheerio.load(html);
+    // var text = $('#app > div > header > div.center-wrap > div.fr > div.util.hidden-sm-down > a').text();
+
+    await driver.findElement(By.css('#app > div > header > div.center-wrap > div.fr > div.util.hidden-sm-down > a')).click();
+    var apiKey = await driver.manage().getCookie('ncp_nnb');
+
+    console.log(apiKey.value);
     // console.log(html)
 }
 example();
