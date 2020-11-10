0b493c910ab1ea9186769c6df14e646b1daa9ed6
Hanseok Kim <hasuikhs@gmail.com>
2020-11-10 14:16:14 +0900

    201110|Update selenium

--- with nodejs/index.js
+++ with nodejs/index.js
@@ -28,6 +28,8 @@ console.log(testff.test());
 
 async function example(userid, userpw) {
     let driver = await new Builder().forBrowser('chrome').build();
+    driver.manage().setTimeouts({implicit: 5000, pageLoad: 5000, script: 5000});
+
 
     await driver.get('https://auth.ncloud.com/nsa/bizspring');
     await driver.findElement(By.id('username')).sendKeys(userid, Key.RETURN);
@@ -38,27 +40,27 @@ async function example(userid, userpw) {
     // var $ = cheerio.load(html);
     // var text = $('#app > div > header > div.center-wrap > div.fr > div.util.hidden-sm-down > a').text();
 
-    await driver.findElement(By.css('#app > div > header > div.center-wrap > div.fr > div.util.hidden-sm-down > a')).click();
+    // await driver.findElement(By.css('#app > div > header > div.center-wrap > div.fr > div.util.hidden-sm-down > a')).click();
     var apiKey = await driver.manage().getCookie('ncp');
-
-    const option = {
-        uri: 'https://monitoring-api.ncloud.com/monapi/pfmnc',
-        method: 'POST',
-        headers : {
-            'X-NCP-access-token' : apiKey.value
-        },
-        body: {
-            "targets": [
-                { 
-                    "metric": "avg.svr.cpu.used.rto",
-                    "name": "avg.svr.cpu.used.rto____0", 
-                    "key": "F2:20:CD:AD:38:85", 
-                    "startTime": "202010191159", 
-                    "endTime": "202010191259" }
-                ]
-        },
-        json: true
-    }
+    console.log(apiKey);
+    // const option = {
+    //     uri: 'https://monitoring-api.ncloud.com/monapi/pfmnc',
+    //     method: 'POST',
+    //     headers : {
+    //         'X-NCP-access-token' : apiKey.value
+    //     },
+    //     body: {
+    //         "targets": [
+    //             { 
+    //                 "metric": "avg.svr.cpu.used.rto",
+    //                 "name": "avg.svr.cpu.used.rto____0", 
+    //                 "key": "F2:20:CD:AD:38:85", 
+    //                 "startTime": "202010191159", 
+    //                 "endTime": "202010191259" }
+    //             ]
+    //     },
+    //     json: true
+    // }
     
     request.post(option, function (error, response, body) {
         console.log(response.body.csv);
