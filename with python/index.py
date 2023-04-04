c98647c47ee46aaf6cfd9f8ad4bb25d879ecea3d
hanseok kim <hasuikhs@gmail.com>
2023-04-04 22:34:57 +0900

    230404|Update progressbar

--- with python/index.py
+++ with python/index.py
@@ -1,8 +1,8 @@
-import os, time
+import os, time, datetime as dt
 from module import Crawler
 from dotenv import load_dotenv
 import pandas as pd
-from util import transform_str_to_int
+from util import transform_str_to_int, print_progress
 
 load_dotenv()
 
@@ -10,7 +10,7 @@ WAIT_TIME = 60
 
 ## 1. input keyword
 INPUT_KEYWORD = input('검색할 키워드 입력: ')
-LIMIT_CNT = input('총 아이디 개수 입력: ')
+LIMIT_CNT = int(input('총 아이디 개수 입력: '))
 LIMIT_FOLLOWER = int(input('팔로우수 하한선 입력: '))
 
 ## 2. open browser
@@ -59,32 +59,32 @@ FOLLOW_ELEMNT = 'span[style="line-height: 18px;"]'
 
 id_set = set([])
 
-main_index = 1
-index = 1
+main_index = 0
+index = 0
 try:
-  while len(id_set) < int(LIMIT_CNT):
-    time.sleep(0.3)
+  while len(id_set) < LIMIT_CNT:
+    time.sleep(0.5)
     id = target.get_located_element(TEXT_ELEMENT).text
     target.hover_element(TEXT_ELEMENT)
-    time.sleep(1.5)
+    time.sleep(2)
     
     try:
       follower = transform_str_to_int(target.get_elements(FOLLOW_ELEMNT)[4].text)
     except:
       follower = transform_str_to_int(target.get_elements(FOLLOW_ELEMNT)[5].text)
 
-    print(follower, LIMIT_FOLLOWER)
-    if follower >= LIMIT_FOLLOWER:
+    if follower >= LIMIT_FOLLOWER and id not in id_set:
       id_set.add(id)
-      df.loc[len(df)] = [index, id, follower, f'https://www.instagram.com/{ id }']
       index = index + 1
+      df.loc[len(df)] = [index, id, follower, f'https://www.instagram.com/{ id }']
 
     main_index = main_index + 1
 
-    print(f'총 시도: { main_index }, 현재 들어간 횟수: { index }')
+    print_progress(index, LIMIT_CNT, main_index, 'Progress:', 'Complete', 1, 50)
     target.submit_arrow_right()
 
 finally:
-  df.to_csv('result.csv')
+  now = dt.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+  df.to_csv(f'result_{ now }.csv')
 
 target.quit_driver()
--- with python/util.py
+++ with python/util.py
@@ -1,3 +1,5 @@
+import sys
+
 def transform_str_to_int(str):
   num = 0
   if '만' in str:
@@ -8,3 +10,14 @@ def transform_str_to_int(str):
     num = float(str)
 
   return num
+
+def print_progress(iteration, total, try_cnt, prefix = '', suffix = '', decimals = 1, bar_length  = 100):
+  format_str = "{0:." + str(decimals) + "f}"
+  percent = format_str.format(100 * (iteration / float(total)))
+  filled_length = int(round(bar_length * iteration / float(total)))
+  bar = '#' * filled_length + '-' * (bar_length - filled_length)
+
+  sys.stdout.write('\r%s |%s| %s%s %s [%s/%s] [try: %s]' % (prefix, bar, percent, '%', suffix, iteration, total, try_cnt)),
+  if iteration == total:
+    sys.stdout.write('\n')
+  sys.stdout.flush()
