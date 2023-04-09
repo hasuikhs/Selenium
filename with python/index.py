a53a46c7cd8c52a20080a1f3d85caad16f067ff6
hanseok kim <hasuikhs@gmail.com>
2023-04-09 17:44:30 +0900

    230409|Update comment

--- with python/index.py
+++ with python/index.py
@@ -46,14 +46,6 @@ PW_SELECTOR = '#loginForm > div > div:nth-child(2) > div > label > input'
 target.login(ID_SELECTOR, PW_SELECTOR, LOGIN_ID, LOGIN_PW)
 
 ## 5. search
-# SEARCH_IMG = 'svg[aria-label="검색"]'
-# SEARCH_INPUT = 'input[aria-label="입력 검색"]'
-
-# el = target.get_located_element(SEARCH_IMG)
-# parent = target.get_parent_element(el)
-# parent.click()
-
-# target.submit_input(SEARCH_INPUT, keyword)
 time.sleep(5)
 SEARCH_URL = f'https://www.instagram.com/explore/tags/{ INPUT_KEYWORD }/'
 target.move_site(SEARCH_URL)
