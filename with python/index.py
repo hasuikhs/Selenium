b7a3c0b71f4427a63bde13c8da9227591ceb1b38
hanseok kim <hasuikhs@gmail.com>
2023-04-10 22:26:09 +0900

    230410|Modify crawler

--- with python/index.py
+++ with python/index.py
@@ -15,7 +15,7 @@ LIMIT_CNT = int(input('총 아이디 개수 입력: '))
 LIMIT_FOLLOWER = int(input('팔로우수 하한선 입력: '))
 
 ## 2. 폴더 생성 및 기존 파일 읽기
-dir_path = os.getcwd() + '/results'
+dir_path = os.getcwd() + '/results/'
 if not os.path.exists(dir_path):
   os.makedirs(dir_path)
 
@@ -57,35 +57,35 @@ target.find_clickable(FIRST_TARGET).click()
 
 ### 6.1 create dataframe
 df = pd.DataFrame({
-  '번호': [],
+  'num': [],
   'id': [],
-  '팔로우': [],
   'link': [],
+  'follwers': []
 })
 
 TEXT_ELEMENT = 'article header div:nth-child(2) > div > div span a'
 FOLLOW_ELEMNT = 'span[style="line-height: 18px;"]'
 
+new_id_set = set(file_df['id'])
 ### 7.searching...
-
 main_index = 0
 index = 0
 try:
-  while len(id_set) < LIMIT_CNT:
-    time.sleep(0.5)
+  while len(new_id_set) < LIMIT_CNT:
+    time.sleep(1)
     id = target.get_located_element(TEXT_ELEMENT).text
     target.hover_element(TEXT_ELEMENT)
-    time.sleep(2)
+    time.sleep(4)
     
     try:
       follower = transform_str_to_int(target.get_elements(FOLLOW_ELEMNT)[4].text)
     except:
       follower = transform_str_to_int(target.get_elements(FOLLOW_ELEMNT)[5].text)
 
-    if follower >= LIMIT_FOLLOWER and id not in id_set:
-      id_set.add(id)
+    if follower >= LIMIT_FOLLOWER and id not in id_set and id not in new_id_set and 'official' not in id:
+      new_id_set.add(id)
       index = index + 1
-      df.loc[len(df)] = [index, id, follower, f'https://www.instagram.com/{ id }']
+      df.loc[len(df)] = [index, id, f'https://www.instagram.com/{ id }', follower]
 
     main_index = main_index + 1
 
@@ -94,7 +94,7 @@ try:
 
 ### 7.1 도중 오류 발생해도 파일 생성
 finally:
-  now = dt.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+  now = dt.datetime.now().strftime('%Y-%m-%d_%H%M')
   df.to_csv(f'results/result_{ INPUT_KEYWORD }_{ now }.csv')
 
 target.quit_driver()
